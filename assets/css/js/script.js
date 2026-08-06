588c2afa4f3e50c3ddff5f390bebe7eb6df4fca0
Prashanthbj-cell <prashanthbj23@gmail.com>
2026-08-07 00:18:41 +0530

    Update script.js

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -102,21 +102,35 @@ function downloadIcon(image, name) {
 
         let ctx = canvas.getContext("2d");
 
+        // Draw the icon
         ctx.drawImage(img, 0, 0);
 
-        let pngFile = canvas.toDataURL("image/png");
+        // ===== WATERMARK =====
+       ctx.save();
+ctx.rotate(-Math.PI / 4);
 
+ctx.font = "80px Arial";
+ctx.fillStyle = "rgba(0, 0, 0, 0.3)";
+ctx.textAlign = "center";
+
+for (let x = -canvas.width; x < canvas.width * 2; x += 300) {
+    for (let y = -canvas.height; y < canvas.height * 2; y += 120) {
+        ctx.fillText("OMICONS", x, y);
+    }
+}
+
+ctx.restore();
+        // =====================
+
+        let pngFile = canvas.toDataURL("image/png");
 
         let link = document.createElement("a");
 
         link.href = pngFile;
-
         link.download = name + ".png";
 
         document.body.appendChild(link);
-
         link.click();
-
         document.body.removeChild(link);
 
     };
