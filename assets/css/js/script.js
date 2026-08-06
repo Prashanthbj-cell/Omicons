97f3b2ecd965341fcbd51000e1b98f70e567f163
Prashanthbj-cell <prashanthbj23@gmail.com>
2026-08-07 00:32:28 +0530

    Update script.js

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -113,7 +113,7 @@ ctx.font = "80px Arial";
 ctx.fillStyle = "rgba(0, 0, 0, 0.3)";
 ctx.textAlign = "center";
 
-for (let x = -canvas.width; x < canvas.width * 2; x += 300) {
+for (let x = -canvas.width; x < canvas.width * 2; x += 350) {
     for (let y = -canvas.height; y < canvas.height * 2; y += 120) {
         ctx.fillText("OMICONS", x, y);
     }
